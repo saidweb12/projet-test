f559f045541399a5bac84faa8c3a4fe2233c3a47
saidweb12 <said-farah@hotmail.com>
2025-02-18 15:05:19 +0100

    modifie le dossier public et templates

--- public/index.php
+++ public/index.php
@@ -1,9 +1,19 @@
 <?php
 if (isset($_GET["p"])) {
     switch ($_GET["p"]) {
+
         case "aproposView":
         include ("../templates/aproposView.php");
         break;
+
+        case "histoireView":
+            include ("../templates/histoireView.php");
+            break;
+            
+            case "contactView":
+                include ("../templates/contactView.php");
+                break;
+               
 
 
         }
