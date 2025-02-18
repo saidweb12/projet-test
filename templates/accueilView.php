2d45a4423fa2c02c1a0edf7dd95d772acfa3c6cb
saidweb12 <said-farah@hotmail.com>
2025-02-18 15:27:48 +0100

    travailler sur nav-bar

--- templates/accueilView.php
+++ templates/accueilView.php
@@ -15,14 +15,11 @@
         </header>
      
 
-        <nav>
-            <ul class="nav">
-                <li> <a href="./">Accueil</a></li>
-                <li><a href="?p=aproposView">Interview</a></li>
-                <li><a href="?p=histoireView">Bruxelles</a></li>
-                <li><a href="?p=contactView">Conclusion</a></li>
-            </ul>
-        </nav>
+       <?php
+       include("inc/menuIncView.php");
+
+
+?>
 
         <main>
            
---
+++ templates/inc/menuIncView.php
@@ -0,0 +1,10 @@
+
+
+<nav>
+            <ul class="nav">
+                <li> <a href="./">Accueil</a></li>
+                <li><a href="?p=aproposView">Interview</a></li>
+                <li><a href="?p=histoireView">Bruxelles</a></li>
+                <li><a href="?p=contactView">Conclusion</a></li>
+            </ul>
+        </nav>
